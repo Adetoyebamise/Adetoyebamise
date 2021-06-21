9fdbcbf597d6b53043757d3e37a3c6dfe38a4d5f
Adetoye Babalola <adetoyebamise@gmail.com>
2021-06-21 13:45:24 +0100

    add funtionality to the cv

--- app.js
+++ app.js
@@ -16,46 +16,92 @@ navNodeList.forEach((navElement) => {
 });
 
 // scroll sections active link
-const sectionsWithId = document.querySelectorAll("section[id]");
+// const sectionsWithId = document.querySelectorAll("section[id]");
 
-const scrollActive = () => {
-  const scrollVertical = window.pageYOffset;
+// const scrollActive = () => {
+//   const scrollVertical = window.pageYOffset;
+//   console.log(scrollVertical);
+//   sectionsWithId.forEach((currentHeight) => {
+//     console.log(currentHeight);
 
-  sectionsWithId.forEach((currentHeight) => {
-    const sectionWithIdHeight = currentHeight.offsetHeight;
-
-    const sectionWithIdTop = currentHeight.offsetTop - 50;
-
-    const sectionId = currentHeight.getAttribute("id");
-
-    if (
-      scrollVertical > sectionWithIdTop &&
-      scrollVertical <= sectionWithIdTop + sectionWithIdHeight
-    ) {
-      const abc = document.querySelector(
-        ".nav__menu a[href*=" + sectionId + "]"
-      );
-      abc.classList.add("active__link");
-    } else {
-      const abc = document.querySelector(
-        ".nav__menu a[href*=" + sectionId + "]"
-      );
-      abc.classList.remove("active__link");
-    }
-  });
-};
-
-window.addEventListener("scroll", scrollActive());
+//     const sectionWithIdHeight = currentHeight.offsetHeight;
+//     console.log(sectionWithIdHeight);
+//     const sectionWithIdTop = currentHeight.offsetTop - 50;
+//     console.log(sectionWithIdTop);
+//     sectionId = currentHeight.getAttribute("id");
+//     console.log(sectionId);
+//     if (
+//       scrollVertical > sectionWithIdTop &&
+//       scrollVertical <= sectionWithIdTop + sectionWithIdHeight
+//     ) {
+//       const abc = document.querySelector(
+//         ".nav__menu a[href*=" + sectionId + "]"
+//       );
+//       console.log(abc);
+//       abc.classList.add("active__link");
+//     } else {
+//       const xyz = document.querySelector(
+//         ".nav__menu a[href*=" + sectionId + "]"
+//       );
+//       console.log(xyz);
+//       xyz.classList.remove("active__link");
+//     }
+//   });
+// };
+//a[href*=sectionId]a[href=]
+// window.addEventListener("scroll", scrollActive());
 
 // show scroll top
 const scrollTop = () => {
-  const scrollTop = document.querySelector("scrolltop");
+  const scrollTopLevel = document.querySelector(".scrolltop");
+  console.log(scrollTopLevel);
   // when the scroll is higher than 560 viewport,
-  //add the show scroll class to the "a" tag
-  if (pageYOffset >= 200) {
-    scrollTop.classList.add("show-scroll");
+  //add the show scroll class to the "a" tag with the scroll-top class
+  if (window.pageYOffset >= 200) {
+    scrollTopLevel.classList.add("show-scroll");
   } else {
-    scrollTop.classList.remove("show-scroll");
+    scrollTopLevel.classList.remove("show-scroll");
   }
 };
-window.addEventListener("scroll", scrollTop);
+window.addEventListener("scroll", scrollTop());
+
+// reduce the size and print on an A4 sheet
+const scaleCv = () => {
+  document.body.classList.add("scale-cv");
+};
+
+// remove the size when the cv is downloaded
+const removeScaledCv = () => {
+  document.body.classList.remove("scale-cv");
+};
+console.log(removeScaledCv);
+
+// generate pdf : pdf generated area
+let areaCv = document.querySelector(".resume");
+console.log(areaCv);
+let resumeButton = document.querySelector(".generate-pdf");
+console.log(resumeButton);
+
+// html2pdf options
+let opt = {
+  margin: 0,
+  filename: "adetoyeBamiseResume.pdf",
+  image: { type: "jpeg", quality: 0.98 },
+  html2canvas: { scale: 2 },
+  jsPDF: { unit: "in", format: "a4", orientation: "portrait" },
+};
+
+// function to call areaCv and HTML2pdf options
+const generateResume = () => {
+  html2pdf(areaCv, opt);
+};
+
+// when button is clicked, it executes the three functions
+resumeButton.addEventListener("click", () => {
+  // 1. the class .scale-cv is added to the body, where it redeuces the  size of the ..
+  scaleCv();
+  // 2. the pdf is generated
+  generateResume();
+  // 3. the scale-cv class is removed from the body after 5 seconds to ..
+  setTimeout(removeScaledCv, 2000);
+});
